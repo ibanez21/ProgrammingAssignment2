fc3a43f322357b0aaec8ce40044a3b41baf10083
Jon Knowles <jgknowles21@gmail.com>
2021-05-17 20:28:12 -0500

    modified with additional comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,13 +5,22 @@
 
 makeCacheMatrix <- function(m = matrix())  {
     inv <- NULL
+    # the function set_matrix will take as its argument a matrix, and 
+    # will update the value returned when get_matrix() is called
     set_matrix <- function(y)  {
         m <<- y
         inv <<- NULL
     }
+    # get_matrix returns the matrix associated with the object
     get_matrix <- function() m
+    # set_inverse accepts a matrix as an argument, and stores the 
+    #result in inv. It is not meant to be called from the object, rather
+    # set_inverse is called in the function below, and the result is placed
+    # in the inv variable above.
     set_inverse <- function(solve) inv <<- solve
+    # get_inverse returns the inverse of the matrix argument m.
     get_inverse <- function() inv
+    # return list of functions for object
     list(set_matrix=set_matrix, 
          get_matrix=get_matrix, 
          set_inverse=set_inverse, 
@@ -24,11 +33,16 @@ makeCacheMatrix <- function(m = matrix())  {
 # the inverse from cache
 
 cacheSolve <- function(x, ...)  {
+    # attempt to retrieve inverse matrix from object x. If inverse is
+    # present, return it from cache without recalculating.
     inv <- x$get_inverse()
     if(!is.null(inv))  {
         message("getting cached data")
         return(inv)
     }
+    # here, the inverse has not been calculated, so we do so by using the 
+    # solve() function, and storing the result in inv variable, which will 
+    # be stored in the x object via the set_inverse() functio above.
     data <- x$get_matrix()
     inv <- solve(data, ...)
     x$set_inverse(inv)
